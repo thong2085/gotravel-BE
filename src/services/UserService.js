65e364d667c48ddfbcfde985c9e394967a02f730
thong2085 <129254706+thong2085@users.noreply.github.com>
2024-02-03 23:29:33 +0700

    update

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,5 @@
 const User = require("../models/UserModel");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { genneralAccessToken, genneralRefreshToken } = require("./JwtService");
 
 const createUser = (newUser) => {
