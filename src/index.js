b859057a1f61a1c503ec5852eb1aba6bf888206e
thong2085 <129254706+thong2085@users.noreply.github.com>
2024-02-13 00:17:30 +0700

    update cors

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "https://gotravel-fe.vercel.app",
+  origin: true,
   credentials: true,
 };
 app.use(cors(corsOptions));
